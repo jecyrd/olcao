ea67a587f2011923b3e3b530901607fc4c8ce76c
James Currie <jecyrd@mail.umkc.edu>
2016-03-28 19:16:33 -0500

    Don't worry about it

--- src/polcao/test/simple.f90
+++ src/polcao/test/simple.f90
@@ -1,6 +1,6 @@
 program gaBlockCyclic
 
-  use MPI
+!  use MPI
 
   implicit none
 
@@ -8,20 +8,27 @@ program gaBlockCyclic
 !#include "global.fh"
 
   integer :: mpiRank, mpiSize, mpierr
+  integer :: i,j
 
   mpiRank = 2
   mpiSize = 0
   mpierr = 0
 
-  call mpi_init(mpierr)
-  print *, "Init: ", mpierr
-  call MPI_COMM_RANK(MPI_COMM_WORLD, mpiRank, mpierr)
-  print *, "Rank: ", mpierr
-  call MPI_COMM_SIZE(MPI_COMM_WORLD, mpiSize, mpierr)
-  print *, "Size: ", mpierr
+!  call mpi_init(mpierr)
+!  print *, "Init: ", mpierr
+!  call MPI_COMM_RANK(MPI_COMM_WORLD, mpiRank, mpierr)
+!  print *, "Rank: ", mpierr
+!  call MPI_COMM_SIZE(MPI_COMM_WORLD, mpiSize, mpierr)
+!  print *, "Size: ", mpierr
+!
+!
+!  print *, "\n\nStuff: ", mpiRank, mpiSize
+!
+!  call mpi_finalize(mpierr)
 
-
-  print *, "\n\nStuff: ", mpiRank, mpiSize
-
-  call mpi_finalize(mpierr)
+  do i=1,4
+    do j=1,4
+      print *, i, j
+    enddo
+  enddo
 end program

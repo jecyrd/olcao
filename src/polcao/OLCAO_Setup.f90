7a6ca829f86c8e721c5c25ee0388a51f2638efe1
Jim Currie <curriej@localhost.localdomain>
2019-05-15 11:12:52 -0500

    Reinstrumented the necessary parallel modifications into OLCAO_Setup

--- src/polcao/OLCAO_Setup.f90
+++ src/polcao/OLCAO_Setup.f90
@@ -29,19 +29,23 @@ subroutine setupSCF
    use O_Potential,   only: cleanUpPotential
    use O_CoreCharge,  only: makeCoreRho
    use O_TimeStamps,  only: initOperationLabels
+   
+   ! Import Parallel Support Modules
+   use O_Parallel
+   use O_ParallelSetup
 
    ! Import the HDF5 module.
    use HDF5
 
    ! Import the necessary MPI files
-!   use MPI
+   use MPI
 
    ! Make sure that there are no accidental variable declarations.
    implicit none
 
    ! Define global mpi parameters
-!   integer :: myWorldPid
-!   integer :: numWorldProcs
+   integer :: mpiRank
+   integer :: mpiSize
 
    ! Define tau parameters
 !   integer profiler(2) / 0, 0 /
@@ -50,13 +54,24 @@ subroutine setupSCF
    ! Define error detectors for hdf, tau, and mpi.
    integer :: hdferr
 !   integer :: tauerr
-!   integer :: mpierr
+   integer :: mpierr
 
+   ! Define cvOL global array data structure, and blacs info data structure
+   type(ArrayInfo) :: cvOLArrayInfo
+   type(BlacsInfo) :: blcsinfo
+
+   ! Define the linked list that will hold the list of atoms read in that
+   !    this process will need to calculate
+   type(AtomPair), pointer :: atomList => null()
+
+   ! Define the process grid variable that will be read in with the atomList.
+   !    This is calculated before hand to make sure we distribute correctly.
+   integer, dimension(2) :: pgrid
 
    ! Initialize the MPI interface
-!   call MPI_INIT (mpierr)
-!   call MPI_COMM_RANK (MPI_COMM_WORLD,myWorldPid,mpierr)
-!   call MPI_COMM_SIZE (MPI_COMM_WORLD,numWorldProcs,mpierr)
+   call MPI_INIT (mpierr)
+   call MPI_COMM_RANK (MPI_COMM_WORLD,mpirank,mpierr)
+   call MPI_COMM_SIZE (MPI_COMM_WORLD,mpisize,mpierr)
 
    ! Initialize tau timer and start it.
 
@@ -105,105 +120,121 @@ subroutine setupSCF
    ! Determine the parameters for the exchange correlation mesh.
    call getECMeshParameters
 
-
    ! Now, the dimensions of the system are known.  Therefor we can
    !   initialize the HDF5 file structure format, and datasets.
-   call initSetupHDF5 (maxNumRayPoints)
-
+   !   Only the process with mpirank==0 will need to open this file,
+   !   we will not do any parallel writes.
+   if (mpiRank == 0) then
+      call initSetupHDF5 (maxNumRayPoints)
+   endif
+   ! Syncronize all processes
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Construct the exchange correlation overlap matrix, and sampling field.
-   call makeECMeshAndOverlap
-
+   call makeECMeshAndOverlap()
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
+   
+   ! Construct matrix operators and integral vectors that are used to later
+   !   determine the electrostatic potential.
+   call makeElectrostatics()
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Create the alpha distance matrices.
-   call makeAlphaDist
+   call makeAlphaDist()
 
+   ! Read in the atom pair lists
+   call accessAtomPairsHDF5(atomList, pgrid)
 
-   ! Allocate space to be used for each of the integrals.  The 1 for the
-   !   valeVale cases is a place holder for these matrices because they will
-   !   later (main.exe) consider spin.
-   call allocateIntegralsSCF(coreDim,valeDim,numKPoints)
+   ! Setup the BLACS interface
+   call setupBlacs(blcsinfo, pgrid)
 
+   ! Setup the array descriptor needed to persist through all the overlap
+   !    routines.
+   call setupArrayDesc(cvOLArrayInfo, blcsinfo, coreDim, valeDim, numKPoints)
 
    ! Calculate the matrix elements of the overlap between all LCAO Bloch
    !   wave functions.
-   call gaussOverlapOL
-
+   call gaussOverlapOL(blcsinfo, cvOLArrayInfo, atomList)
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Calculate the matrix elements of the kinetic energy between all LCAO Bloch
    !   wave functions.
-   call gaussOverlapKE
-
-
+   call gaussOverlapKE(blcsinfo, cvOLArrayInfo, atomList)
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Create the alpha distance matrix with nuclear alpha factor
-   call makeAlphaNucDist
+   call makeAlphaNucDist()
 
    ! Calculate the matrix elements of the overlap between all LCAO Bloch
    !   wave functions and the nuclear potentials.
-   call gaussOverlapNP
-
-
+   call gaussOverlapNP(blcsinfo, cvOLArrayInfo, atomList)
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Create the alpha distance matrix with potential alpha factor
-   call makeAlphaPotDist
+   call makeAlphaPotDist()
 
    ! Calculate the matrix elements of the overlap between all LCAO Bloch
    !   wave functions and the potential site potential alphas.
-   call elecPotGaussOverlap
+   call elecPotGaussOverlap(blcsinfo, cvOLArrayInfo, atomList)
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Now that all the matrices are done being made we can deallocate the
    !   data structures that were used in all the above subroutines but are not
    !   necessary now.
-   call cleanUpIntegralsSCF
-   call cleanUpBasis
-   call cleanUpGaussRelations
+   call cleanUpBasis()
+   call cleanUpGaussRelations()
 
 
    ! Construct a vector describing the core charge density since it will not
    !   change throughout the SCF iterations because of core orthogonalization.
-   if (coreDim /= 0) then
-      call makeCoreRho
+   if ((coreDim /= 0) .and. (mpiRank == 0)) then
+      call makeCoreRho()
    endif
-
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
 
    ! Deallocate the data component of the atomType data structure that holds
    !   the radial functions since they are no longer needed.
-   call cleanUpRadialFns
-
-
-   ! Construct matrix operators and integral vectors that are used to later
-   !   determine the electrostatic potential.
-   call makeElectrostatics
+   call cleanUpRadialFns()
 
    ! Close all the parts of the setup HDF5 file.
-   call closeSetupHDF5
+   if (mpiRank == 0) then
+      call closeSetupHDF5()
+   endif
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
+
+   ! Deallocate the CVoL matrix
+   call deallocLocalArray(cvOLArrayInfo)
 
    ! Close the HDF5 interface.
    call h5close_f (hdferr)
    if (hdferr /= 0) stop 'Failed to close the HDF5 interface.'
 
    ! Deallocate all the other as of yet un-deallocated arrays.
-   call cleanUpAtomTypes
-   call cleanUpAtomSites
-   call cleanUpPotTypes
-   call cleanUpPotSites
-   call cleanUpKPoints
-   call cleanUpExchCorr
-   call cleanUpLattice
-   call cleanUpPotential
+   call cleanUpAtomTypes()
+   call cleanUpAtomSites()
+   call cleanUpPotTypes()
+   call cleanUpPotSites()
+   call cleanUpKPoints()
+   call cleanUpExchCorr()
+   call cleanUpLattice()
+   call cleanUpPotential()
 
    ! End the tau timer
 !   call TAU_PROFILE_STOP(profiler)
 
-   ! End the MPI interface
-!   call MPI_FINALIZE (mpierr)
 
    ! Close the output file
    close (20)
 
    ! Open a file to signal completion of the program.
-   open (unit=2,file='fort.2',status='unknown')
+   if (mpiRank == 0) then
+      open (unit=2,file='fort.2',status='unknown')
+      close (2)
+   endif
+   call MPI_Barrier(MPI_COMM_WORLD, mpierr)
+   
+   ! End the MPI interface
+   call MPI_FINALIZE (mpierr)
 
 end subroutine setupSCF
 
